d9532bcbbc4485dc0ef4fa01dce97eed6ca6083d
Chintan <cpandya231@github.com>
2019-10-16 23:10:04 +0530

    First commit on globo(a react native app)
    - By this commmit I am starting my new project on react native.
    - added some sample components and setup react native app

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './app/views/Home';
+
+export default function App() {
+  return (
+    <Home></Home>
+  );
+}
---
+++ app/views/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export class Home extends React.Component {
+    render() {
+        return (<View >
+            <Text>Home Component works!</Text>
+        </View>)
+    }
+}
